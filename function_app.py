a556adc4788b25044305260e14b0b2f1e45ab0d7
Jean Scheider <108657558+Jeanget@users.noreply.github.com>
2025-12-07 10:55:36 +0000

    build: updated azure function_app

--- function_app.py
+++ function_app.py
@@ -3,23 +3,26 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
+from prediction import make_prediction
+
 @app.route(route="ruby_predict")
 def ruby_predict(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    tenure = req.params.get('tenure')
+    monthly = req.params.get('monthly')
+    techsupport = req.params.get('techsupport')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    prediction = make_prediction(
+    tenure=tenure,
+    MonthlyCharges=monthly,
+    TechSupport_yes=techsupport
+    )
+
+    if tenure and monthly and techsupport:
+        return func.HttpResponse(f"For the given tenure, monthly and techsupport, the predicted churn is {prediction}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. tenure, monthly and techsupport.",
              status_code=200
         )
