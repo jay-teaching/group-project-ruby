6954c0900fca43ea6c09d59160d31c52952e1d9b
achahal9 <arjun.chahal@esmt.berlin>
2025-12-12 13:40:45 +0000

    fix: pytest prediction

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -7,5 +7,17 @@ def test_make_prediction_simple():
     This test must be modified as per the actual model used.
 
     """
-    result = prediction.make_prediction(tenure=2, MonthlyCharges=12.3, TechSupport_yes=0)
+    payload = {
+        "Dependents_yes": 0,
+        "TechSupport_yes": 0,
+        "Contract_one year": 1,
+        "Contract_two year": 0,
+        "OnlineBackup_yes": 1,
+        "OnlineSecurity_yes": 0,
+        "InternetService_fiber optic": 0,
+        "DeviceProtection_yes": 1,
+        "tenure": 2,
+        "MonthlyCharges": 12.3,
+    }
+    result = prediction.make_prediction(**payload)
     assert isinstance(result, float)
